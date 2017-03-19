73888d74b2b0a2200240124e5a79ad4e136531d9
Kuldeep Sharma <kuldeepsharma1312@gmail.com>
2017-03-19 19:45:21 +0530

    Inbuilt+my_implementation of Naive Bayes

--- TC_NB.py
+++ TC_NB.py
@@ -13,68 +13,102 @@ import numpy as np
 import scipy as sp
 import sklearn
 
+
+
 ###########################################################################
-						# Loading data	
+			# Loading data	
 
 results = []
-with open('inputfile.txt') as inputfile:
+with open('trainfile.txt') as inputfile:
     for line in inputfile:
         results.append(line.strip().split('\t'))
 
-tag =[];data=[];
+train_tag =[];train_data=[];
 for i in range(len(results)):
 	if(len(results[i])>1):
 		if(results[i][0]=="student"):
-			tag.append(0)
-			data.append(results[i][1])
+			train_tag.append(0)
+			train_data.append(results[i][1])
 		elif(results[i][0]=="faculty"):
-			tag.append(1)
-			data.append(results[i][1])
+			train_tag.append(1)
+			train_data.append(results[i][1])
 		elif(results[i][0]=="project"):
-			tag.append(2)
-			data.append(results[i][1])
+			train_tag.append(2)
+			train_data.append(results[i][1])
 		elif(results[i][0]=="course"):
-			tag.append(3)
-			data.append(results[i][1])			
-	
+			train_tag.append(3)
+			train_data.append(results[i][1])	
+
+with open('testfile.txt') as inputfile:
+    for line in inputfile:
+        results.append(line.strip().split('\t'))
+
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer()
-train_data=vectorizer.fit_transform(data)
-k=4;
-m ,d = train_data.shape
-label = np.zeros((k,m))
-for i in range(m):
-	label[tag[i],i]=1
-weights=np.zeros((k,d)); # weights[i,j]= prob. of jth word of dict. belongs to ith class
+train_data=vectorizer.fit_transform(train_data)        
+
+test_tag =[];test_data=[];
+for i in range(len(results)):
+	if(len(results[i])>1):
+		if(results[i][0]=="student"):
+			test_tag.append(0)
+			test_data.append(results[i][1])
+		elif(results[i][0]=="faculty"):
+			test_tag.append(1)
+			test_data.append(results[i][1])
+		elif(results[i][0]=="project"):
+			test_tag.append(2)
+			test_data.append(results[i][1])
+		elif(results[i][0]=="course"):
+			test_tag.append(3)
+			test_data.append(results[i][1])					
+	
+
+test_data=vectorizer.transform(test_data)
+
+k=4; # Number of classes
+
+train_label = np.zeros((k,train_data.shape[0]))
+test_label = np.zeros((k,test_data.shape[0]))
+for i in range(train_data.shape[0]):
+	train_label[train_tag[i],i]=1
+for i in range(test_data.shape[0]):
+	test_label[test_tag[i],i]=1
+
+
+weights=np.zeros((k,train_data.shape[1])); # weights[i,j]= prob. of jth word of dict. belongs to ith class
 prior=np.zeros((k,1)); # prior[i] = prob. of being ith class
 
 
-################################################################################
-						# Divideing the data in 2 part 
+#################################################################################
+				# Trinaing the Model
 
-data = train_data[:1500,:]
-test_data = train_data[1500:,:]
-labels = label[:,:1500]
-test_label = label[:,1500:]
+weights = ((train_label*train_data)+1)/(np.reshape(np.sum(train_label,axis=1)+k,[k,1]));
+prior = (np.sum(train_label,axis=1)/train_data.shape[0]);
+
+				# Using In-built Naive Bayes
+
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(train_data,train_tag)
+
 
 #################################################################################
-						# Trinaing the Model
-
-weights = ((labels*data)+1)/(np.reshape(np.sum(labels,axis=1)+k,[k,1]));
-prior = (np.sum(labels,axis=1)/m);
-
-#################################################################################
-						# Testing and calculating accuracy
-count=0;
+				# Testing and calculating accuracy
+count=0;count_in=0;
 for i_i in range(test_label.shape[1]):
-
 	temp = weights*np.transpose(test_data[i_i,:])
 	prob=np.zeros((k,1))
 	for i in range(4):
 		prob[i,0] = prior[i]*temp[i] 
 	if((test_label[np.argmax(prob),i_i]==1.0)):
 		count+=1
+	if((test_label[clf.predict(test_data[i_i,:]),i_i]==1.0)):
+		count_in+=1;
 
-print "accuracy is...  ",(count/1285.0)*100," %" 	
+
+print "accuracy is (using my Implementation of Naive Bayes)...  ",(count/float(test_label.shape[1]))*100," %" 
+print "Accusrcy is (using in built Naive Bayes)...  ",(count_in/float(test_label.shape[1]))*100, " %"	
+
+###################################################################################
 
 ###################################################################################
