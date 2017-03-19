3596bc4b17c3c67bcfebf007482a10d0af65b688
Kuldeep Sharma <kuldeepsharma1312@gmail.com>
2017-03-19 18:33:00 +0530

    Update in NB program

--- TC_NB.py
+++ TC_NB.py
@@ -1,72 +1,80 @@
-#####################################################
-#													#	
-#	Text Classification (Document Classification)	#
-#													#
-#	using -- Naive Bayes							#
-#													#		
-#	Kuldeep Sharma (20/03/2017)						#
-#													#	
-#	Github --	    								#
-#####################################################
-
-import numpy as np 
-
-#def load_data(filename):
-
-	# This is to load data set	
-
-def train_model(data,labels,weights,prior):
-
-	# This is to train my model
-	m = data.shape[0];
-	d = data.shape[1];
-	k = labels.shape[1];
-
-	# This is one way, using for loops
-	# for i_ in range(d):
-	# 	for i in range(k):
-	# 		num=0;
-	# 		den=0;
-	# 		for j in range(m):
-	# 			num = num + labels[i,j]*data[j,i_]; 
-	# 			den = den + labels[i,j];
-	# 		weights[i,i_] = (num+1)/(den+k);	# Here I am also handling the laplace smoothing	 
-	# 		prior[i,1]=den/m;
-
-	# Second way, vectorization in python (Much Faster)		
-
-	weights = (np.dot(labels,data)+1)/(np.reshape(np.sum(labels,axis=1)+k,[k,1]));
-	prior = (np.sum(labels,axis=1)/m);
-
-def test_model(input_, weights, prior):
-
-	# This is to test my model on given input
-
-	prob = prior * (np.dot(weights,input_));
-	for i in range(weights.shape[0]):
-		print prob[i]
-
-	print "Label is: ",np.argmax(prob),"."	
-
-if __name__=="__main__": 
-
-	d=100; # dimension of the features vector (This is also equal to size of dictionary)
-	m=1000; # number of training example
-	k=5; # number of classes
-
-	# Load Data will be called here once we have dataset
-	data = np.random.rand(m,d); # This is my data set for the time being
-	labels = np.random.rand(k,m); # Labels of the given dataset
-
-	weights=np.zeros((k,d)); # weights[i,j]= prob. of jth word of dict. belongs to ith class
-	prior=np.zeros((k,1)); # prior[i] = prob. of being ith class
-
-	train_model(data,labels,weights,prior);
-
-	input_ = np.random.rand(d,1);
-
-	test_model(input_,weights,prior);
+#########################################################
+#														#
+#	Text Classification using Naive Bayes  				#
+#	Kuldeep Sharma (kuldeepsharma1312@gmail.com)		#
+#	Mechanical Engineer @ IIT Delhi						#
+#	Github -- @Kuldeep_Attri							#
+#														#
+#########################################################
 
 
 
+import numpy as np
+import scipy as sp
+import sklearn
 
+###########################################################################
+						# Loading data	
+
+results = []
+with open('inputfile.txt') as inputfile:
+    for line in inputfile:
+        results.append(line.strip().split('\t'))
+
+tag =[];data=[];
+for i in range(len(results)):
+	if(len(results[i])>1):
+		if(results[i][0]=="student"):
+			tag.append(0)
+			data.append(results[i][1])
+		elif(results[i][0]=="faculty"):
+			tag.append(1)
+			data.append(results[i][1])
+		elif(results[i][0]=="project"):
+			tag.append(2)
+			data.append(results[i][1])
+		elif(results[i][0]=="course"):
+			tag.append(3)
+			data.append(results[i][1])			
+	
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer()
+train_data=vectorizer.fit_transform(data)
+k=4;
+m ,d = train_data.shape
+label = np.zeros((k,m))
+for i in range(m):
+	label[tag[i],i]=1
+weights=np.zeros((k,d)); # weights[i,j]= prob. of jth word of dict. belongs to ith class
+prior=np.zeros((k,1)); # prior[i] = prob. of being ith class
+
+
+################################################################################
+						# Divideing the data in 2 part 
+
+data = train_data[:1500,:]
+test_data = train_data[1500:,:]
+labels = label[:,:1500]
+test_label = label[:,1500:]
+
+#################################################################################
+						# Trinaing the Model
+
+weights = ((labels*data)+1)/(np.reshape(np.sum(labels,axis=1)+k,[k,1]));
+prior = (np.sum(labels,axis=1)/m);
+
+#################################################################################
+						# Testing and calculating accuracy
+count=0;
+for i_i in range(test_label.shape[1]):
+
+	temp = weights*np.transpose(test_data[i_i,:])
+	prob=np.zeros((k,1))
+	for i in range(4):
+		prob[i,0] = prior[i]*temp[i] 
+	if((test_label[np.argmax(prob),i_i]==1.0)):
+		count+=1
+
+print "accuracy is...  ",(count/1285.0)*100," %" 	
+
+###################################################################################
